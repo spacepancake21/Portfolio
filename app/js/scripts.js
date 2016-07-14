4510af2f06e734659bb82b454c27067e7ef1a4d0
Konstantin <davydov_k@mail.ru>
2016-07-14 15:51:38 +0300

    blog page ready

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -30,15 +30,49 @@ $(function() {
 
 	})();
 
-	// Активация меню
-
 	(function() {
 
-		// Переворачивание плашек на главной странице
+		// Активация меню
 
 		$('.js-menu-toggle').on('click', function() {
 			$(this).toggleClass('header__menu_active');
 			$('.menu').toggleClass('menu_active');
+		});
+
+	})();
+
+	(function() {
+
+		// Активация сайдбара
+
+		$('.js-sidebar-toggle').on('click', function() {
+			$(this).parent().toggleClass('blog__sidebar_active');
+			$('.header').toggleClass('header_slide-right');
+			$('.blog__content').toggleClass('blog__content_slide-right');
+		});
+
+		//
+
+		$(window).on('resize', function() {
+			if($(window).width() >= 1200) {
+				$('.blog__sidebar').removeClass('blog__sidebar_active')
+					.removeClass('blog__sidebar_fixed');
+				$('.header').removeClass('header_slide-right');
+				$('.blog__content').removeClass('blog__content_slide-right');
+			}
+		});
+
+		//
+
+		var blogSidebar = $('.blog__sidebar'),
+		sidebarOffset = blogSidebar.offset().top;
+
+		$(document).on('scroll', function() {
+			if($(window).scrollTop() > sidebarOffset) {
+				blogSidebar.addClass('blog__sidebar_fixed');
+			} else {
+				blogSidebar.removeClass('blog__sidebar_fixed');
+			}
 		});
 
 	})();

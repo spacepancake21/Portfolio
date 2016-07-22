3172ba174a543891c33f5837823a2c2f7155706a
Konstantin <davydov_k@mail.ru>
2016-07-22 16:48:30 +0300

    works page ready (no js slider)

--- app/js/about.js
+++ app/js/about.js
@@ -1,50 +1,59 @@
 "use strict";
 
-function initMap() {
+$(function() {
 
-	var customMapType = new google.maps.StyledMapType([
-		{
-			stylers: [
-				{hue: '#eae8e2'},
-				{visibility: 'simplified'},
-				{saturation: -200},
-				{gamma: 3},
-				{weight: 0}
-			]
-		},
-		{
-			featureType: 'water',
-			stylers: [{color: '#4369aa'}]
-		}
-	], {
-		name: 'Custom Style'
-	});
+	function initMap() {
 
-	var customMapTypeId = 'custom_style';
+		var customMapType = new google.maps.StyledMapType([
+			{
+				stylers: [
+					{hue: '#eae8e2'},
+					{visibility: 'simplified'},
+					{saturation: -200},
+					{gamma: 3},
+					{weight: 0}
+				]
+			},
+			{
+				featureType: 'water',
+				stylers: [{color: '#4369aa'}]
+			}
+		], {
+			name: 'Custom Style'
+		});
 
-	var myLatLng = {lat: 55.606226, lng: 37.531561};
+		var customMapTypeId = 'custom_style';
 
-	// Create a map object and specify the DOM element for display.
-	var map = new google.maps.Map(document.getElementById('map'), {
-		center: myLatLng,
-		scrollwheel: false,
-		//disableDefaultUI: true,
-		zoom: 12,
-		mapTypeControlOptions: {
-			mapTypeIds: [google.maps.MapTypeId.ROADMAP, customMapTypeId]
-		}
-	});
+		var myLatLng = {lat: 55.606226, lng: 37.531561};
 
-	map.mapTypes.set(customMapTypeId, customMapType);
-	map.setMapTypeId(customMapTypeId);
+		// Create a map object and specify the DOM element for display.
+		var map = new google.maps.Map(document.getElementById('map'), {
+			center: myLatLng,
+			scrollwheel: false,
+			//disableDefaultUI: true,
+			zoom: 12,
+			mapTypeControlOptions: {
+				mapTypeIds: [google.maps.MapTypeId.ROADMAP, customMapTypeId]
+			}
+		});
 
-	// Create a marker and set its position.
-	var image = '../img/svg/map_marker.svg';
-	var marker = new google.maps.Marker({
-		icon: image,
-		map: map,
-		position: myLatLng
-	});
-}
+		map.mapTypes.set(customMapTypeId, customMapType);
+		map.setMapTypeId(customMapTypeId);
+
+		// Create a marker and set its position.
+		var image = '../img/svg/map_marker.svg';
+		var marker = new google.maps.Marker({
+			icon: image,
+			map: map,
+			position: myLatLng
+		});
+	}
 
 
+	if($('#map').length) {
+
+		initMap();
+
+	}
+
+});

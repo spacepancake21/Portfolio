c51d6847a9fc40557fd5a20adbe676f54892c832
Konstantin <davydov_k@mail.ru>
2016-08-05 11:43:11 +0300

    preloader fixes

--- app/js/preloader.js
+++ app/js/preloader.js
@@ -28,37 +28,37 @@ $(function() {
 
 		}
 
-		var percents = 1;
-
-		for(var i = 0; i < imgs.length; i++) {
-
-			var image = $('<img>', {
-				attr: {
-					src: imgs[i]
-				}
-			});
-
-			image.load(function() {
-				setPercents(imgs.length, percents);
-				percents++;
-			});
-
-		}
-
-		function setPercents(total, current) {
-
-			var percent = Math.ceil(current / total * 100);
-
-			$('.preloader__percent').text(percent);
-
-			if(percent >= 100) {
-				$('.wrapper').css('visibility', 'visible');
-				$('.footer').css('display', 'block');
-				$('.preloader').delay(1000).fadeOut(1500);
-			}
-
-		}
-
 	});
+
+	var percents = 1;
+
+	for(var i = 0; i < imgs.length; i++) {
+
+		var image = $('<img>', {
+			attr: {
+				src: imgs[i]
+			}
+		});
+
+		image.on('load', function() {
+			setPercents(imgs.length, percents);
+			percents++;
+		});
+
+	}
+
+	function setPercents(total, current) {
+
+		var percent = Math.ceil(current / total * 100);
+
+		$('.preloader__percent').text(percent);
+
+		if(percent >= 100) {
+			$('.wrapper').css('display', 'block');
+			$('.footer').css('display', 'block');
+			$('.preloader').delay(1000).fadeOut(1500);
+		}
+
+	}
 
 });
